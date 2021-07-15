0b1c50eff36d97bd539d1da275b99ffdef779b5e
Vayugundla Girish Guptha <girishvayugundla@gmail.com>
2021-07-16 00:01:45 +0530

    Returning all perumutations of the string

--- AlgorithmProblems/Program.cs
+++ AlgorithmProblems/Program.cs
@@ -6,7 +6,21 @@ namespace AlgorithmProblems
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("***Algorithm Problems***");
+            Console.WriteLine("1.Permutation of string");
+            Console.Write("Enter your choice:");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    Console.Write("Enter The  String : ");
+                    string str = Console.ReadLine();
+                    int length = str.Length;
+                    Permutation.permute(str, 0, length - 1);
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
